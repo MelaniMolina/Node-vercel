58110181f9eba5088a34a8d214878648540ebd9f
MelaniMolina <melani.molina@epn.edu.ec>
2023-06-04 19:32:18 -0500

    nuevos

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/',(req,res)=>{
-    res.send('Pagina de Inicio Taller Numero 8')
+    res.send("Landing page - Grupo #")
+})
+
+app.get('/integrantes',(req,res)=>{
+    res.json([
+        {
+            "nombre":"Byron",
+            "apellido":"Loarte",
+            "edad":34
+        },
+        {
+            "nombre":"Juan",
+            "apellido":"Álvear",
+            "edad":38
+        },
+        {
+            "nombre":"Ximena",
+            "apellido":"Salazar",
+            "edad":33
+        }
+    ])
+})
+
+app.get('/products',(req,res)=>{
+    res.send(`
+            <h1>Catálogo de productos</h1>
+                <p>Bienvenidos</p>
+    `)
 })
 
 app.listen(port);
