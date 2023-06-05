0c5bb99fb06f30d8ea52207f2d6f91153017ba4f
MelaniMolina <melani.molina@epn.edu.ec>
2023-06-04 19:53:31 -0500

    cambios_finales

--- index.js
+++ index.js
@@ -39,73 +39,81 @@ app.get('/integrantes',(req,res)=>{
 
 app.get('/products',(req,res)=>{
     res.send(`
-            <h1>Catálogo de productos</h1>
-                <p>Bienvenidos</p>
-                
-                   <style>
-                        body {
-                            text-align: center;
-                        }
-                
-                        h1 {
-                            color: #333;
-                        }
-                
-                        .product-list {
-                            list-style-type: none;
-                            padding: 0;
-                            margin: 20px 0;
-                        }
-                
-                        .product-list li {
-                            margin-bottom: 10px;
-                        }
-                
-                        .product-list li span {
-                            display: inline-block;
-                            width: 100px;
-                            text-align: left;
-                        }
-                
-                        .btn {
-                            display: inline-block;
-                            padding: 10px 20px;
-                            background-color: #333;
-                            color: #fff;
-                            text-decoration: none;
-                            margin: 10px;
-                        }
-                
-                        .btn:hover {
-                            background-color: #555;
-                        }
-                    </style>
-                <body>
-                    <h1>Catálogo de productos</h1>
-                    <p>Bienvenidos</p>
-                
-                    <ul class="product-list">
-                        <li>
-                            <span>Producto 1:</span>
-                            <span>$10.00</span>
-                        </li>
-                        <li>
-                            <span>Producto 2:</span>
-                            <span>$15.00</span>
-                        </li>
-                        <li>
-                            <span>Producto 3:</span>
-                            <span>$20.00</span>
-                        </li>
-                        <!-- Agrega más productos aquí -->
-                
-                    </ul>
-                
-                    <a href="#" class="btn">Guardar compra</a>
-                    <a href="#" class="btn">Cancelar compra</a>
-                </body>
-                </html>
-                
+   
+       
+            body {
+                text-align: center;
+                background-color: #f2f9f0;
+            }
+    
+            h1 {
+                color: #207e4c;
+                font-family: 'Arial', sans-serif;
+                font-size: 24px;
+            }
+    
+            .product-list {
+                list-style-type: none;
+                padding: 0;
+                margin: 20px 0;
+                text-align: center;
+            }
+    
+            .product-list li {
+                margin-bottom: 10px;
+            }
+    
+            .product-list li span {
+                display: inline-block;
+                width: 100px;
+                text-align: left;
+                color: #207e4c;
+                font-family: 'Arial', sans-serif;
+                font-size: 16px;
+            }
+    
+            .btn {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #207e4c;
+                color: #fff;
+                text-decoration: none;
+                margin: 10px;
+                font-family: 'Arial', sans-serif;
+                font-size: 18px;
+                border-radius: 5px;
+            }
+    
+            .btn:hover {
+                background-color: #0d5533;
+            }
+        </style>
+   
+   
+        <h1>Catálogo de productos</h1>
+        <p>Bienvenidos</p>
+    
+        <ul class="product-list">
+            <li>
+                <span>Producto 1:</span>
+                <span>$10.00</span>
+            </li>
+            <li>
+                <span>Producto 2:</span>
+                <span>$15.00</span>
+            </li>
+            <li>
+                <span>Producto 3:</span>
+                <span>$20.00</span>
+            </li>
+            <!-- Agrega más productos aquí -->
+    
+        </ul>
+    
+        <a href="#" class="btn">Guardar compra</a>
+        <a href="#" class="btn">Cancelar compra</a>
+   
+        
     `)
 })
 
