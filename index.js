71fc1bce4c6cdc3b62fb5dd8dd682c01fc407235
MelaniMolina <melani.molina@epn.edu.ec>
2023-06-04 19:07:30 -0500

    nuevos

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.get('/',(req,res)=>{
+    res.send('Pagina de Inicio Taller Numero 8')
+})
+
 app.listen(port);
 console.log(`Listen en Port ${port}`);
