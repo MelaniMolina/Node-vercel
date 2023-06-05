cad4f33527cfb81fd0f8275b4bf737069fad5e32
MelaniMolina <melani.molina@epn.edu.ec>
2023-06-04 19:44:40 -0500

    fin

--- index.js
+++ index.js
@@ -4,25 +4,35 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/',(req,res)=>{
-    res.send("Landing page - Grupo #")
+    res.send("Landing page - Grupo 2")
 })
 
 app.get('/integrantes',(req,res)=>{
     res.json([
         {
-            "nombre":"Byron",
-            "apellido":"Loarte",
-            "edad":34
+            "nombre":"Wilson",
+            "apellido":"Guayanay",
+            "edad":20
         },
         {
-            "nombre":"Juan",
-            "apellido":"Álvear",
-            "edad":38
-        },
-        {
-            "nombre":"Ximena",
+            "nombre":"Josue",
             "apellido":"Salazar",
-            "edad":33
+            "edad":20
+        },
+        {
+            "nombre":"Marlon",
+            "apellido":"Lalangui",
+            "edad":20
+        },
+        {
+            "nombre":"Melani",
+            "apellido":"Molina",
+            "edad":20
+        },
+        {
+            "nombre":"Katherine",
+            "apellido":"Cangahuamin",
+            "edad":24
         }
     ])
 })
@@ -31,6 +41,71 @@ app.get('/products',(req,res)=>{
     res.send(`
             <h1>Catálogo de productos</h1>
                 <p>Bienvenidos</p>
+                
+                   <style>
+                        body {
+                            text-align: center;
+                        }
+                
+                        h1 {
+                            color: #333;
+                        }
+                
+                        .product-list {
+                            list-style-type: none;
+                            padding: 0;
+                            margin: 20px 0;
+                        }
+                
+                        .product-list li {
+                            margin-bottom: 10px;
+                        }
+                
+                        .product-list li span {
+                            display: inline-block;
+                            width: 100px;
+                            text-align: left;
+                        }
+                
+                        .btn {
+                            display: inline-block;
+                            padding: 10px 20px;
+                            background-color: #333;
+                            color: #fff;
+                            text-decoration: none;
+                            margin: 10px;
+                        }
+                
+                        .btn:hover {
+                            background-color: #555;
+                        }
+                    </style>
+                <body>
+                    <h1>Catálogo de productos</h1>
+                    <p>Bienvenidos</p>
+                
+                    <ul class="product-list">
+                        <li>
+                            <span>Producto 1:</span>
+                            <span>$10.00</span>
+                        </li>
+                        <li>
+                            <span>Producto 2:</span>
+                            <span>$15.00</span>
+                        </li>
+                        <li>
+                            <span>Producto 3:</span>
+                            <span>$20.00</span>
+                        </li>
+                        <!-- Agrega más productos aquí -->
+                
+                    </ul>
+                
+                    <a href="#" class="btn">Guardar compra</a>
+                    <a href="#" class="btn">Cancelar compra</a>
+                </body>
+                </html>
+                
     `)
 })
 
